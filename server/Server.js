d26d0d6981c241c011e6c262da3199a981e1662f
Isidro Paniagua <Isidro.Paniagua@jamulcasinosd.com>
2022-08-14 18:39:14 -0700

    pushing backend data

---
+++ server/Server.js
@@ -0,0 +1,54 @@
+require('dotenv/config')
+
+const io = require('socket.io')(process.env.PORT, {
+  cors: {
+    origin: '*'
+  }
+})
+
+let calledNumbers = []
+
+// Initializes Socket IO
+io.on("connection", (socket) => {
+  console.log(socket.id)
+
+  // When the page initially loads, send server data.
+  socket.on('homepage-request-numbers', () => {
+    io.emit('get-numbers-homepage', calledNumbers);
+  })
+
+  socket.on('controls-request-numbers', () => {
+    io.emit('get-numbers-controls', calledNumbers);
+  })
+
+  // This handles numbers being sent
+  socket.on('request-send-number', (number) => {
+    let newList = calledNumbers
+    newList.push(number)
+    calledNumbers = newList
+    console.log(calledNumbers)
+    io.emit('send-number', number)
+    socket.broadcast.emit('update-numbers', calledNumbers);
+  })
+    // This handles numbers being removed
+  socket.on('request-remove', number => {
+    let newList = calledNumbers
+    const index = newList.indexOf(number)
+    newList.splice(index, 1)
+    calledNumbers = newList
+    console.log(calledNumbers)
+    socket.broadcast.emit('update-numbers', calledNumbers);
+    io.emit('remove-number', number)
+  })
+    // This handles board clears
+    socket.on('request-clear', () => {
+      calledNumbers = []
+      socket.broadcast.emit('update-numbers', calledNumbers);
+      io.emit('clear-board')
+    })
+    // This handles bingo calls
+  socket.on('request-call-bingo', () => {
+    io.emit('call-bingo')
+  })
+})
+
